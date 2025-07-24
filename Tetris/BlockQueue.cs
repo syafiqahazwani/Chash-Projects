cf2cb887b7f796f7baf6f62cd7ce6f9d04b3ebb0
NURSYAFIQAH AZWANI BINTI MOHD NIZAM <azwani.nizam@bestinet.com.my>
2025-07-24 11:41:36 +0800

    Project-2-tetris-game

---
+++ Tetris/BlockQueue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class BlockQueue
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new OBlock(),
+            new TBlock(),
+            new SBlock(),
+            new ZBlock(),
+            new JBlock(),
+            new LBlock()
+        };
+
+        private readonly Random random = new Random();
+
+        public Block NextBlock { get; private set; }
+
+        public BlockQueue()
+        {
+            NextBlock = RandomBlock();
+        }
+
+        private Block RandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+
+            do
+            {
+                NextBlock = RandomBlock();
+            }
+            while (block.Id == NextBlock.Id);
+
+            return block;
+        }
+    }
+}
---
+++ Tetris/Position.cs
@@ -0,0 +1,13 @@
+﻿namespace Tetris
+{
+    public class Position
+    {
+        public int Row { get; }
+        public int Column { get; }
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
